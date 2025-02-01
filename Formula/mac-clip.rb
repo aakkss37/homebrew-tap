ed0008b6fef12c487a8b6b62fd8042fee23607b7
Amar kumar <amarkumar.sharma.124@hmail.com>
2025-02-01 12:00:27 +0530

    mac-clip: update to 0.2.0

--- Formula/mac-clip.rb
+++ Formula/mac-clip.rb
@@ -1,9 +1,9 @@
 class MacClip < Formula
   desc "A clipboard history manager for macOS"
   homepage "https://github.com/aakkss37/mac-clip"
-  url "https://github.com/aakkss37/mac-clip/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "26d63440463aed2fd7302a4b6e9679272b2600fdda33d6b2004d3f5c520e38c5"
-  version "0.1.0"
+  url "https://github.com/aakkss37/mac-clip/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "2280edea6f31276beebb46618c75649cab10bc08d4beaddb27b0457d37eda952"
+  version "0.2.0"
   license "MIT"
 
   depends_on "rust" => :build
